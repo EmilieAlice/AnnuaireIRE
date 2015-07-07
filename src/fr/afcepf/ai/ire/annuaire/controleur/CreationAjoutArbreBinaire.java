93feef71924bd1df78087147cea6e8f37a3940d6
rosane-ai95 <rosane.tan@gmail.com>
2015-07-07 13:24:49 +0200

    m

--- src/fr/afcepf/ai/ire/annuaire/controleur/CreationAjoutArbreBinaire.java
+++ src/fr/afcepf/ai/ire/annuaire/controleur/CreationAjoutArbreBinaire.java
@@ -44,11 +44,7 @@ public class CreationAjoutArbreBinaire extends Stagiaire  {
 		try {
 
 			InputStream isr = new FileInputStream(
-<<<<<<< HEAD
 					"C:/Users/Stagiaire/Desktop/STAGIAIRESTEST.DON");
-=======
-					"C:/Users/Stagiaire/Desktop/STAGIAIRES.DON");
->>>>>>> origin/master
 			InputStreamReader fr = new InputStreamReader(isr,
 					StandardCharsets.UTF_8);
 			BufferedReader br = new BufferedReader(fr);
@@ -88,7 +84,7 @@ public class CreationAjoutArbreBinaire extends Stagiaire  {
 					formaterEnRubrique(unStagiaire, fichierAStructurer);
 
 					//TEST OK OK OK
-					//System.out.println("Numero Stagiaire : " + numeroLigne);
+					System.out.println("Numero Stagiaire : " + numeroLigne);
 
 					//TEST OK
 					//System.out.println(LONGUEURLIGNE);
@@ -102,7 +98,7 @@ public class CreationAjoutArbreBinaire extends Stagiaire  {
 				numeroLigne+=1;
 
 				//TEST OK OK OK
-			
+				//System.out.println(unStagiaire.donneUnStagiaireEntier());
 				//System.out.println(numeroLigne);
 
 			}
@@ -235,7 +231,7 @@ public class CreationAjoutArbreBinaire extends Stagiaire  {
 			int fils = 0;
 
 			//TEST OK OK OK
-			//System.out.println("\t" + nomStagiaireFils + "/" + nomDuPere + " --> chiffre de comparaison : " + chiffreDeComparaison);
+			System.out.println("\t" + nomStagiaireFils + "/" + nomDuPere + " --> chiffre de comparaison : " + chiffreDeComparaison);
 
 			//
 			if(chiffreDeComparaison<0){
@@ -289,9 +285,9 @@ public class CreationAjoutArbreBinaire extends Stagiaire  {
 		}
 
 		//TEST OK OK OK
-//		System.out.println("\t" + "LectureChampsFils : " + champsFilsDuPere);
-//		System.out.println("\t" + "pere du stagiaire : "+ unStagiaire.getChampsPere());
-//		System.out.println();
+		System.out.println("\t" + "LectureChampsFils : " + champsFilsDuPere);
+		System.out.println("\t" + "pere du stagiaire : "+ unStagiaire.getChampsPere());
+		System.out.println();
 		fichierAStructurer.seek(LONGUEURLIGNE*numeroDeLigne);
 		//FIN TEST
 
@@ -310,7 +306,7 @@ public class CreationAjoutArbreBinaire extends Stagiaire  {
 			lecture += fichierAStructurer.readChar();
 			i++;
 		}
-		//System.out.println(lecture);
+		System.out.println(lecture);
 	}
 
 }
